a7c120a790a9b90416d26bbc4099ae7d53f6fffb
evanp-c <44926220+evanp-c@users.noreply.github.com>
2022-01-19 12:35:28 -0500

    Add files via upload

---
+++ Lab1Code/src/main.cpp
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\evanp                                            */
+/*    Created:      Wed Jan 19 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Right                motor         10              
+// Left                 motor         1               
+// Arm                  motor         8               
+// RangeFinderE         sonar         E, F            
+// BumperC              bumper        C               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+using namespace vex;
+
+const double wheelDiameter = 10; // CM
+//const double GearRatio = 1/5;
+//const double WheelTrack = 31.5;
+
+
+void move(double distance) {
+  cout << "hi";
+  double circumference = wheelDiameter * M_PI;
+
+
+  Right.spinFor(5*distance*360/circumference, degrees, false);
+  Left.spinFor(5*distance*360/circumference, degrees);
+}
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  task::sleep(250);
+  move(10.0);
+
+}
+
+
---
+++ Lab1Code/src/robot-config.cpp
@@ -0,0 +1,29 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+motor Right = motor(PORT10, ratio18_1, false);
+motor Left = motor(PORT1, ratio18_1, true);
+motor Arm = motor(PORT8, ratio18_1, false);
+sonar RangeFinderE = sonar(Brain.ThreeWirePort.E);
+bumper BumperC = bumper(Brain.ThreeWirePort.C);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
